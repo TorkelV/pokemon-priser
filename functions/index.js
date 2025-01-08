91556d92af03f36bdfff7cf59a64831796215ca1
TorkelV <torkel93@gmail.com>
2025-01-08 20:27:53 +0100

    run function and invalidate files on deploy

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,17 @@
-import functions from '@google-cloud/functions-framework';
-import { Storage } from '@google-cloud/storage';
+const functions = require('@google-cloud/functions-framework');
+const {Storage} = require('@google-cloud/storage');
 const storage = new Storage();
-import { products } from './products.js';
-import { scrapeAllProducts } from './scraper.js';
+const {products} = require('./products.js');
+const {scrapeAllProducts} = require('./scraper.js');
+const {ComputeManagementClient} = require('@google-cloud/compute');
+
 
 async function saveJsonToBucket(bucketName, json, filename) {
     try {
         const filePath = `${filename}.json`;
         const bucket = storage.bucket(bucketName);
         const file = bucket.file(filePath);
-        await file.save(json, { metadata: { contentType: 'application/json' }, resumable: true });
+        await file.save(json, {metadata: {contentType: 'application/json'}, resumable: true});
         console.log(`JSON data saved to gs://${bucketName}/${filePath}`);
         return true;
     } catch (error) {
@@ -18,14 +20,50 @@ async function saveJsonToBucket(bucketName, json, filename) {
     }
 }
 
+const invalidateCache = async () => {
+    try {
+        const computeClient = new ComputeManagementClient();
+        const project = 'pokemon-priser'; //Your Project ID
+        const urlMap = 'pokemon-priser'; //Your URL Map Name
+
+        // Construct the request
+        const request = {
+          project: project,
+          urlMap: urlMap,
+          paths: ['/master.json'], //Paths to invalidate
+        };
+
+        //Invalidate CDN cache
+        const [operation] = await computeClient.urlMaps.invalidateCache(request);
+
+        //Wait for the operation to complete
+        console.log('Waiting for the operation to complete...');
+        await operation.promise();
+        console.log('CDN cache invalidated successfully!');
+
+
+    } catch (cdnError) {
+        console.error("Error invalidating CDN cache:", cdnError);
+        //Handle CDN invalidation failure appropriately (e.g., retry, alert)
+    }
+    return Promise.resolve()
+}
+
 functions.http('getPrices', async (req, res) => {
     const bucketName = 'pokemon-priser-data';
     const mapped = await scrapeAllProducts(products);
     const json = JSON.stringify(mapped);
     const timestamp = Date.now();
     const masterSaveSuccess = await saveJsonToBucket(bucketName, json, 'master');
+
+    const invalidate = invalidateCache()
+
     const uniqueFilename = `history/data-${timestamp}`;
     const uniqueSaveSuccess = await saveJsonToBucket(bucketName, json, uniqueFilename);
+    await invalidate
     res.sendStatus(200);
-    return true
+    return true;
 });
+
+
+
