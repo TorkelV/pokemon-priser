46719bd11b0de16fccb6057f642b9c36e123facd
TorkelV <torkel93@gmail.com>
2025-02-16 23:28:28 +0100

    proshop headers

--- functions/parsers.js
+++ functions/parsers.js
@@ -229,6 +229,20 @@ export const parsers = {
 }
 
 export const headers = {
-    "https://proshop.no": {"User-Agent": "Googlebot-Image"},
+    "https://proshop.no": {   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
+        "Accept-Encoding": "gzip, deflate, br, zstd", 
+        "Accept-Language": "en-US,en;q=0.9", 
+        "Host": "proshop.no", 
+        "Priority": "u=0, i", 
+        "Referer": "https://www.google.com/", 
+        "Sec-Ch-Ua": "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"", 
+        "Sec-Ch-Ua-Mobile": "?0", 
+        "Sec-Ch-Ua-Platform": "\"macOS\"", 
+        "Sec-Fetch-Dest": "document", 
+        "Sec-Fetch-Mode": "navigate", 
+        "Sec-Fetch-Site": "cross-site", 
+        "Sec-Fetch-User": "?1", 
+        "Upgrade-Insecure-Requests": "1", 
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",},
     "https://norli.no": { "User-Agent": "googlebot" }
 }
